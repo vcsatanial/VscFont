7d23cef69ad507a34b61d694168920ed1f152efd
zhouhongchen <116359398@qq.com>
2017-06-20 11:01:39 +0800

    添加注释

--- VscFont/VscFonts.h
+++ VscFont/VscFonts.h
@@ -10,11 +10,32 @@
 #import <UIKit/UIKit.h>
 
 @interface VscFonts : NSObject<UIAlertViewDelegate>
+
+/**
+ 根据PostScriptName在Block中获取到Font
+ 
+ @param fontName 字体的PostScriptName
+ @param chsName 中文名称(如果有的话  出现alert时会展示,也会展示上面的fontName)
+ @param fontSize 字体大小
+ @param complete 得到font的block
+ @param failure 未能得到font的block
+ */
 +(void)vsc_fontWithPostScriptName:(NSString *)fontName
                           chsName:(NSString *)chsName
                          fontSize:(CGFloat)fontSize
                          complete:(void (^)(UIFont *font))complete
                           failure:(void (^)(NSError *error))failure;
+
+/**
+ 根据PostScriptName在Block中获取到Font
+
+ @param fontName 字体的PostScriptName
+ @param chsName 中文名称(如果有的话  出现alert时会展示,也会展示上面的fontName)
+ @param fontSize 字体大小
+ @param progress 如果需要下载的话,返回下载进度
+ @param complete 得到font的block
+ @param failure 未能得到font的block
+ */
 +(void)vsc_fontWithPostScriptName:(NSString *)fontName
                           chsName:(NSString *)chsName
                          fontSize:(CGFloat)fontSize
